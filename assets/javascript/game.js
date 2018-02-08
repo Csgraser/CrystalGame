e2d408768aa9af1e069911c6ca0e324c0c3b5d92
Csgraser <csgraser@gmail.com>
2018-02-07 22:15:41 -0700

    first upload

---
+++ assets/javascript/game.js
@@ -0,0 +1,15 @@
+$( document ).ready(function(){
+	var random = Math.floor(Math.random()*101+19)
+
+var number1 = Math.floor(Math.random()*11+1)
+var number2 = Math.floor(Math.random()*11+1)
+var number3 = Math.floor(Math.random()*11+1)
+var number4 = Math.floor(Math.random()*11+1)
+var total = 0;
+var wins = 0;
+var losses = 0;
+
+$("#wins").text(wins);
+$("losses").text(losses);
+
+});
